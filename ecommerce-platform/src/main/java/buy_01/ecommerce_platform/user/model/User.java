762a80a2b65dc385b8d17a13178874a442d30fad
Mirak29 <abdoukarimsow363@gmail.com>
2024-09-28 23:35:22 +0000

    validation

--- ecommerce-platform/src/main/java/buy_01/ecommerce_platform/user/model/User.java
+++ ecommerce-platform/src/main/java/buy_01/ecommerce_platform/user/model/User.java
@@ -3,14 +3,27 @@ package buy_01.ecommerce_platform.user.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Document(collection = "users")
 public class User {
     @Id
     private String id;
+
+    @NotBlank(message = "Name is mandatory")
     public String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
     public String email;
+
+    @NotBlank(message = "Password is mandatory")
     private String password;
+
+    @NotBlank(message = "Role is mandatory")
     private String role;
+
     private String avatar;
 
     // Constructors, getters, and setters
