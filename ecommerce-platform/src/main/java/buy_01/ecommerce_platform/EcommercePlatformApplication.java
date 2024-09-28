08ed5933108e61b7512badd906aa7649b0f2063f
Mirak29 <abdoukarimsow363@gmail.com>
2024-09-28 12:42:12 +0000

    init microservices

--- ecommerce-platform/src/main/java/buy_01/ecommerce_platform/EcommercePlatformApplication.java
+++ ecommerce-platform/src/main/java/buy_01/ecommerce_platform/EcommercePlatformApplication.java
@@ -2,8 +2,14 @@ package buy_01.ecommerce_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = {
+    "buy_01.ecommerce_platform.user.repository",
+    "buy_01.ecommerce_platform.product.repository",
+    "buy_01.ecommerce_platform.media.repository"
+})
 public class EcommercePlatformApplication {
 
 	public static void main(String[] args) {
--- ecommerce-platform/src/main/java/buy_01/ecommerce_platform/product/ProductServiceApplication.java
+++ ecommerce-platform/src/main/java/buy_01/ecommerce_platform/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package buy_01.ecommerce_platform.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:product-service.properties")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
