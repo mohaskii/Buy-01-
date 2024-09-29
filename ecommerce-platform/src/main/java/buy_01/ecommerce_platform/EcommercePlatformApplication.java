697ee53a1d02057add264adef83ec7e5fecc3a95
Mirak29 <abdoukarimsow363@gmail.com>
2024-09-29 05:41:44 +0000

    user pswd encrypting and user register handler

--- ecommerce-platform/src/main/java/buy_01/ecommerce_platform/EcommercePlatformApplication.java
+++ ecommerce-platform/src/main/java/buy_01/ecommerce_platform/EcommercePlatformApplication.java
@@ -2,7 +2,11 @@ package buy_01.ecommerce_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {
@@ -10,10 +14,10 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
     "buy_01.ecommerce_platform.product.repository",
     "buy_01.ecommerce_platform.media.repository"
 })
+@ComponentScan(basePackages = {"buy_01.ecommerce_platform", "buy_01.ecommerce_platform.config"})
 public class EcommercePlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercePlatformApplication.class, args);
 	}
-
 }
